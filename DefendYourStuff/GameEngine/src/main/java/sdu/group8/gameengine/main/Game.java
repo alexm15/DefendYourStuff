651ab063636cd5f0475fefe11f4ddc7577a715e6
alexm15 <alexander.markussen@live.dk>
2017-03-13 14:31:11 +0100

    Copied and cleaned classes from Core module in AstroidsProject

--- DefendYourStuff/GameEngine/src/main/java/sdu/group8/gameengine/main/Game.java
+++ DefendYourStuff/GameEngine/src/main/java/sdu/group8/gameengine/main/Game.java
@@ -1,16 +1,125 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package sdu.group8.gameengine.main;
 
-/**
- *
- * @author Alexander
- */
-public class Game 
-{
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.openide.util.Lookup;
 
+/**
+ * 
+ * @author Group8
+ */
+public class Game
+        implements ApplicationListener {
+
+    private static OrthographicCamera cam;
+    private ShapeRenderer sr;
+
+    private final GameData gameData = new GameData();
+    private World world = new World();
+    private Lookup lookup = Lookup.getDefault();
+    private List<IGameProcess> gameProcesses = new ArrayList<>();
+    private List<IGamePlugin> gamePlugins = new ArrayList<>();
+    
+
+    @Override
+    public void create() {
+
+        gameData.setDisplayWidth(Gdx.graphics.getWidth());
+        gameData.setDisplayHeight(Gdx.graphics.getHeight());
+
+        cam = new OrthographicCamera(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        cam.translate(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
+        cam.update();
+
+        sr = new ShapeRenderer();
+
+        Gdx.input.setInputProcessor(
+                new GameInputProcessor(gameData)
+        );
+        
+        for (IGamePluginService gamePlugin : getGamePlugins()) {
+            gamePlugin.start(gameData, world);
+        }
+    }
+
+    public Collection<? extends IGameProcess> getGameProcesses() {
+        return lookup.lookupAll(IGameProcess.class);
+    }
+
+    public Collection<? extends IGamePlugin> getGamePlugins() {
+        return lookup.lookupAll(IGamePlugin.class);
+    }
+
+    @Override
+    public void render() {
+
+        // clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        gameData.setDelta(Gdx.graphics.getDeltaTime());
+
+        update();
+
+        draw();
+
+        gameData.getKeys().update();
+    }
+
+    private void update() {
+        // Update
+
+        for (IGameProcess gameProcess : getGameProcesses()) {
+            gameProcess.process(gameData, world);
+        }
+//        playerProcessor.process(gameData, world);
+
+    }
+
+    private void draw() {
+        for (Entity entity : world.getEntities()) {
+            float[] shapex = entity.getShapeX();
+            float[] shapey = entity.getShapeY();
+            if (shapex != null && shapey != null) {
+
+                sr.setColor(1, 1, 1, 1);
+
+                sr.begin(ShapeRenderer.ShapeType.Line);
+
+                for (int i = 0, j = shapex.length - 1;
+                        i < shapex.length;
+                        j = i++) {
+
+                    sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
+                }
+                sr.end();
+            }
+        }
+    }
+
+//    public void addSystem(IEntityProcessingService entityProcess, IGamePluginService gamePlugin) {
+//        gamePlugin.start(gameData, world);
+//        entityProcessors.add(entityProcess);
+//    }
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
