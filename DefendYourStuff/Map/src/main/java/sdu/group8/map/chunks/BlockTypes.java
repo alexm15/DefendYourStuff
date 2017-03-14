c1236553d234bd7b032b3fc5756153f2c943c377
alexm15 <alexander.markussen@live.dk>
2017-03-14 13:24:54 +0100

    Added BlockTypes, Installer to module

---
+++ DefendYourStuff/Map/src/main/java/sdu/group8/map/chunks/BlockTypes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdu.group8.map.chunks;
+
+/**
+ *
+ * @author Alexander
+ */
+public enum BlockTypes {
+    AIR, EARTH, BLACKSMITH, CASTLE, WELL, TOWER, WALL, FARM, PORTAL, RUBBLE,
+    PROPABILITY, WALLTOWER
+    //TODO: Add more propability blocks. 
+    
+}
