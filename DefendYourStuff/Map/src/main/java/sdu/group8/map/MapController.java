241a9691b19ba677b66aee423bbc7d8f6c902831
alexm15 <alexander.markussen@live.dk>
2017-03-20 15:45:58 +0100

    Added map controller/generator for future map generation and loading

--- DefendYourStuff/Map/src/main/java/sdu/group8/map/MapController.java
+++ DefendYourStuff/Map/src/main/java/sdu/group8/map/MapController.java
@@ -6,11 +6,43 @@
 
 package sdu.group8.map;
 
+import sdu.group8.common.data.GameData;
+import sdu.group8.common.data.World;
+import sdu.group8.common.entity.Chunk;
+import static sdu.group8.common.entity.ChunkTypes.*;
+import sdu.group8.common.services.IGamePluginService;
+import sdu.group8.common.services.IGameProcessingService;
+import sdu.group8.map.chunks.CastleChunk;
+import sdu.group8.map.chunks.LeftBaseChunk;
+import sdu.group8.map.chunks.RightBaseChunk;
+
+
 /**
  *
  * @author Alexander
  */
-public class MapController 
+public class MapController implements IGamePluginService, IGameProcessingService
 {
+
+    @Override
+    public void start(GameData gameData, World world) {
+        Chunk castleChunk = new CastleChunk(CASTLE_CHUNK);
+        Chunk leftBaseChunk = new LeftBaseChunk(LEFT_BASE_CHUNK);
+        Chunk rightBaseChunk = new RightBaseChunk(RIGHT_BASE_CHUNK);
+        
+        world.addChunk(leftBaseChunk);
+        world.addChunk(rightBaseChunk);
+        world.addChunk(castleChunk);
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeAllChunks();
+    }
+
+    @Override
+    public void process(GameData gameData, World world) {
+        
+    }
 
 }
