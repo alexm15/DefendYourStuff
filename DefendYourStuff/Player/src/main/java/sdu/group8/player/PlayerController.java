d8de600431fb0a92e16c354765887a0429a9ea87
Alexander Markussen <alexander.markussen@live.dk>
2017-03-23 20:17:42 +0100

    Refactored code: made descriptive private methods

--- DefendYourStuff/Player/src/main/java/sdu/group8/player/PlayerController.java
+++ DefendYourStuff/Player/src/main/java/sdu/group8/player/PlayerController.java
@@ -30,79 +30,87 @@ import sdu.group8.common.services.IGameProcessingService;
 
 public class PlayerController implements IGameProcessingService, IGamePluginService {
 
-    Player player;
+    private Player player;
     private float verticalVelocity;
-    private GameData gd;
 
     @Override
     public void process(GameData gameData, World world) {
-        this.gd = gameData;
+
         if (player.getHealth() == 0) {
             //TODO: remove player from world. Set isGameOver in gameData.           
         }
-        //Handle gravity for player
-        if (!isPlayerOnGround(player)) {
-            player.setPosition(player.getX(), player.getY() + verticalVelocity * gameData.getDelta());
-            verticalVelocity -= gameData.getGRAVITY();
-        } else {
-            verticalVelocity = 0;
-        }
-       
-        //Handle input:
-        
-        //Mouse input.
-        player.setAimPoint(gameData.getCursorPosition());
-        
-        if (gameData.getKeys().isKeyPressed(gameData.getKeys().MOUSE_LEFT)) {
-            //TODO: handle mouse click.
-            System.out.println("left mouse clicked, on pos: " + player.getAimPoint());
-        
-        } else if (gameData.getKeys().isKeyPressed(gameData.getKeys().MOUSE_RIGHT)) {
-            //TODO: handle mouse click.
-            System.out.println("right mouse clicked, on pos: " + player.getAimPoint());
-        
-        } else if (gameData.getKeys().isKeyPressed(gameData.getKeys().MOUSE_MIDDEL)) {
-            //TODO: handle mouse middel click.
-            System.out.println("Middel mouse clicked, on pos: " + player.getAimPoint());
-        }
+        handleGravity(gameData);
 
-        //Keybord input
+        handleMouseInput(gameData);
+
+        handleKeyboardInput(gameData);
+    }
+
+    private void handleKeyboardInput(GameData gameData) {
         if (gameData.getKeys().isKeyDown(gameData.getKeys().D)) {
             player.setPosition(player.getX() + (player.getMoveSpeed() * gameData.getDelta()), player.getY());
-        
+
         } else if (gameData.getKeys().isKeyDown(gameData.getKeys().A)) {
             player.setPosition(player.getX() - (player.getMoveSpeed() * gameData.getDelta()), player.getY());
         }
         if (gameData.getKeys().isKeyPressed(gameData.getKeys().W)) {
-            if (isPlayerOnGround(player)) {
-                setPlayerOnGround(player); //Set player to ground level if player is under ground
+            if (isPlayerOnGround(player, gameData)) {
+                setPlayerOnGround(player, gameData); //Set player to ground level if player is under ground
                 verticalVelocity += player.getVerticalForce();
                 player.setPosition(player.getX(), player.getY() + verticalVelocity * gameData.getDelta());
             }
         }
     }
-    
-/**
- * Is used to finde out if the player is on the ground OR under the ground.
- * @param player the player that will be checked.
- * @return true if the player is on the ground OR under the ground, else false.
- */
-    private boolean isPlayerOnGround(Player player) {
-        if (player.getPosition().getY() <= gd.getGROUND_HEIGHT() + player.getHeight() / 2) {
+
+    private void handleGravity(GameData gameData) {
+        if (!isPlayerOnGround(player, gameData)) {
+            player.setPosition(player.getX(), player.getY() + verticalVelocity * gameData.getDelta());
+            verticalVelocity -= gameData.getGRAVITY();
+        } else {
+            verticalVelocity = 0;
+        }
+    }
+
+    private void handleMouseInput(GameData gameData) {
+        player.setAimPoint(gameData.getCursorPosition());
+        if (gameData.getKeys().isKeyPressed(gameData.getKeys().MOUSE_LEFT)) {
+            //TODO: handle mouse click.
+            System.out.println("left mouse clicked, on pos: " + player.getAimPoint());
+
+        } else if (gameData.getKeys().isKeyPressed(gameData.getKeys().MOUSE_RIGHT)) {
+            //TODO: handle mouse click.
+            System.out.println("right mouse clicked, on pos: " + player.getAimPoint());
+
+        } else if (gameData.getKeys().isKeyPressed(gameData.getKeys().MOUSE_MIDDEL)) {
+            //TODO: handle mouse middel click.
+            System.out.println("Middel mouse clicked, on pos: " + player.getAimPoint());
+        }
+    }
+
+    /**
+     * Is used to finde out if the player is on the ground OR under the ground.
+     *
+     * @param player the player that will be checked.
+     * @return true if the player is on the ground OR under the ground, else
+     * false.
+     */
+    private boolean isPlayerOnGround(Player player, GameData gameData) {
+        if (player.getPosition().getY() <= gameData.getGROUND_HEIGHT() + player.getHeight() / 2) {
             return true;
         }
         return false;
     }
-    
-/**
- * Sets the player to ground level.
- * @param player 
- */
-    private void setPlayerOnGround(Player player){
-        player.setPosition(player.getPosition().getX(), (gd.getGROUND_HEIGHT() + player.getHeight() / 2));
-                
+
+    /**
+     * Sets the player to ground level.
+     *
+     * @param player
+     */
+    private void setPlayerOnGround(Player player, GameData gameData) {
+        player.setPosition(player.getPosition().getX(), (gameData.getGROUND_HEIGHT() + player.getHeight() / 2));
+
     }
-    
+
     @Override
     public void start(GameData gameData, World world) {
         float health = 100;
@@ -121,7 +129,7 @@ public class PlayerController implements IGameProcessingService, IGamePluginServ
         DamageRange damageRange = new DamageRange(minDamage, maxDamage);
         Ability ability = new Ability(position, AOE, damageRange); //TODO: Should be a predifined ability.
         player = new Player(moveSpeed, weight, health, dimension, position, collision, ability);
-        gameData.setPlayerGold(0); 
+        gameData.setPlayerGold(0);
         world.addCharacter(player);
     }
 
