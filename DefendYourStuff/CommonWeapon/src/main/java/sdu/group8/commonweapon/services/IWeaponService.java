299142d1f38ff74c4fc2e724be3994827454ff59
karim møller <Karimkuku@gmail.com>
2017-03-28 15:59:02 +0200

    java doc

--- DefendYourStuff/CommonWeapon/src/main/java/sdu/group8/commonweapon/services/IWeaponService.java
+++ DefendYourStuff/CommonWeapon/src/main/java/sdu/group8/commonweapon/services/IWeaponService.java
@@ -13,7 +13,26 @@ import sdu.group8.common.weapon.Weapon;
  * @author Martin
  */
 public interface IWeaponService {
+    
+    /**
+     * Generates every available weapon that ca be bought 
+     * @return  Returns every available weapon in the game that can be bought. 
+     */
     ArrayList<? extends Weapon> generateWeapons();
+    
+    /**
+     * Generates weapons from a specific class  
+     * @param <W>
+     * @param weaponClass
+     * @return returns every type of the specific weapon
+     */
     <W extends Weapon> ArrayList<W> generateWeapons(Class<W> weaponClass);
+    
+    /**
+     *  returns the class of weapon 
+     * @param <W>
+     * @param weapon
+     * @return The class weapon
+     */
     <W extends Weapon> Class<W> getClass(W weapon);
 }
