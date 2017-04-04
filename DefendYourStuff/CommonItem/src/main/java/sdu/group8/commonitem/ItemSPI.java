b9637696550637239274ddc51b47e94d9d566333
Joachim Hejlesen <johej15@student.sdu.dk>
2017-04-04 14:09:26 +0200

    Updated ItemSPI and added IItemAction

--- DefendYourStuff/CommonItem/src/main/java/sdu/group8/commonitem/ItemSPI.java
+++ DefendYourStuff/CommonItem/src/main/java/sdu/group8/commonitem/ItemSPI.java
@@ -8,6 +8,7 @@ package sdu.group8.commonitem;
 import java.util.Collection;
 import java.util.UUID;
 import sdu.group8.common.data.GameData;
+import sdu.group8.common.data.Position;
 import sdu.group8.common.data.World;
 import sdu.group8.common.entity.Entity;
 import sdu.group8.common.entity.Item;
@@ -31,4 +32,15 @@ public interface ItemSPI {
      * @return a specific item using an ID      
      */
     <I extends Item> I getItem(World world, UUID id);
+    
+    /**
+     * Spawns an item
+     */
+    <I extends Item> I spawnItem();
+    
+    /**
+     * Spawns an item at a specific positioning
+     * @param pos 
+     */
+    void spawnItem(Position pos);
 }
