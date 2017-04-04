8ff095ec1fd6392e67331298ca33abcf8aea0c37
Joachim Hejlesen <johej15@student.sdu.dk>
2017-04-04 14:10:25 +0200

    Added AbilityTypes and AbilitySPI and IAbilityAction

---
+++ DefendYourStuff/CommonAbility/src/main/java/abilityTypes/RangedAbility.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abilityTypes;
+
+import sdu.group8.common.ability.Ability;
+import sdu.group8.common.data.DamageRange;
+import sdu.group8.common.data.Position;
+
+/**
+ *
+ * @author joach
+ */
+public class RangedAbility extends Ability {
+    
+    public RangedAbility(Position position, float AOE, DamageRange damageRange) {
+        super(position, AOE, damageRange);
+    }
+    
+}
