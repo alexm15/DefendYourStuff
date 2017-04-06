5b02beedbae39a0782bbf0e18078b2ea03769315
Minerr <jack.corp@hotmail.com>
2017-04-06 14:32:45 +0200

    Removed all uneccessary methods in World.

--- DefendYourStuff/Common/src/main/java/sdu/group8/common/data/World.java
+++ DefendYourStuff/Common/src/main/java/sdu/group8/common/data/World.java
@@ -5,43 +5,24 @@
  */
 package sdu.group8.common.data;
 
-
-import sdu.group8.common.entity.Building;
-import sdu.group8.common.entity.Item;
-import sdu.group8.common.entity.Projectile;
-import sdu.group8.common.entity.EntityType;
-
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import sdu.group8.common.entity.BlockTypes;
 import sdu.group8.common.entity.Entity;
 import sdu.group8.common.entity.Chunk;
-import sdu.group8.common.entity.ChunkTypes;
 
 /**
  *
  * @author Martin
  */
-public class World<E extends Entity, P extends Projectile, B extends Building, CHUNK extends Chunk> {
+public class World {
 
-    private Map<UUID, E> entitíes = new ConcurrentHashMap<>();
-    private Map<UUID, P> projectiles = new ConcurrentHashMap<>();
-    private Map<UUID, B> buildings = new ConcurrentHashMap<>();
-    private Map<UUID, Item> items = new ConcurrentHashMap<>();
+    private Map<UUID, Entity> entitíes = new ConcurrentHashMap<>();
 
     //For MovingEntities
-      
-    
-    
-    
-    
 //    
-
 //    public Chunk getChunk(ChunkTypes chunkType) {
 //        for (CHUNK chunk : getGameMap()) {
 //            
@@ -53,15 +34,15 @@ public class World<E extends Entity, P extends Projectile, B extends Building, C
 //        return null;
 //    }
     // For Entities
-    public Collection<E> getEntities() {
+    public Collection<Entity> getEntities() {
         return entitíes.values();
     }
 
-    public Collection<E> getEntity(ArrayList<EntityType> entityTypes) {
-        Collection<E> r = new ArrayList<>();
-        for (E entity : getEntities()) {
-            for (EntityType entityType : entityTypes) {
-                if (entity.getEntityType().equals(entityType)) {
+    public <E extends Entity> Collection<Entity> getEntities(Class<E>... entityTypes) {
+        Collection<Entity> r = new ArrayList<>();
+        for (Entity entity : getEntities()) {
+            for (Class<E> entityType : entityTypes) {
+                if (entityType.equals(entityType)) {
                     r.add(entity);
                 }
             }
@@ -70,7 +51,7 @@ public class World<E extends Entity, P extends Projectile, B extends Building, C
         return r;
     }
 
-    public void addEntity(E entity) {
+    public void addEntity(Entity entity) {
         entitíes.put(entity.getID(), entity);
 
     }
@@ -79,91 +60,11 @@ public class World<E extends Entity, P extends Projectile, B extends Building, C
         entitíes.remove(entityID);
     }
 
-    public void removeEntity(E entity) {
+    public void removeEntity(Entity entity) {
         entitíes.remove(entity.getID());
     }
-  
-
-
-    //For Buildings
-    public Collection<B> getBuildings() {
-        return buildings.values();
-    }
-
-    public Collection<B> getBuildings(ArrayList<EntityType> entityTypes) {
-        Collection<B> r = new ArrayList<>();
-        for (B building : getBuildings()) {
-            for (EntityType entityType : entityTypes) {
-                if (building.getEntityType().equals(entityType)) {
-                    r.add(building);
-                }
-            }
-        }
-
-        return r;
-    }
-
-    public void addBuilding(B entity) {
-        buildings.put(entity.getID(), entity);
-
-    }
-
-    public void removeBuilding(UUID entityID) {
-        buildings.remove(entityID);
-    }
-
-    public void removeBuilding(B entity) {
-        buildings.remove(entity.getID());
-    }
-
-    //For items
-    public Collection<Item> getItems() {
-        return items.values();
-    }
-
-    public void addItem(Item item) {
-        items.put(item.getID(), item);
-
-    }
-
-    public void removeItem(UUID entityID) {
-        items.remove(entityID);
-    }
-
-    public void removeItem(Item entity) {
-        items.remove(entity.getID());
-    }
-
-    //For projectiles
-    public Collection<P> getProjectiles() {
-        return projectiles.values();
-    }
-
-    public Collection<P> getProjectiles(ArrayList<EntityType> entityTypes) {
-        Collection<P> r = new ArrayList<>();
-        for (P projectile : getProjectiles()) {
-            for (EntityType entityType : entityTypes) {
-                if (projectile.getEntityType().equals(entityType)) {
-                    r.add(projectile);
-                }
-            }
-        }
-
-        return r;
-    }
     
-    public void addProjectile(P projectile) {
-        projectiles.put(projectile.getID(), projectile);
-
+    public Entity getEntity(UUID entityID) {
+        return entitíes.get(entityID);
     }
-
-    public void removeProjectile(UUID entityID) {
-        projectiles.remove(entityID);
-    }
-
-    public void removeProjectile(P entity) {
-        items.remove(entity.getID());
-    }
-
-    
 }
