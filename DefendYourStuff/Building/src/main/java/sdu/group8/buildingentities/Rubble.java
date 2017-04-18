6dd24214ebfdc2ce91d84a0fbb20eabbbe8f908e
alexm15 <alexander.markussen@live.dk>
2017-04-18 15:21:16 +0200

    Buildings become rubble when destroyed + fixed bug in world.getEntities Also added png's for Castle and Rubble

---
+++ DefendYourStuff/Building/src/main/java/sdu/group8/buildingentities/Rubble.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sdu.group8.buildingentities;
+
+import sdu.group8.common.ability.Ability;
+import sdu.group8.common.data.Dimension;
+import sdu.group8.common.data.Position;
+import sdu.group8.common.entity.Building;
+import sdu.group8.common.entity.BuildingType;
+import sdu.group8.common.entity.CollisionType;
+import sdu.group8.common.entity.Entity;
+import sdu.group8.commonbuilding.services.IBuildingService;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Rubble extends Building implements IBuildingService
+{
+
+    public Rubble(String imageURL, Dimension dimension, Position pos, CollisionType collisionType, BuildingType buildingType, boolean isAttackable, int upgradeLevel, float health, Ability... ab) {
+        super(imageURL, dimension, pos, collisionType, buildingType, isAttackable, upgradeLevel, health, ab);
+    }
+
+    @Override
+    public void collision(Entity otherEntity) {
+    }
+
+    @Override
+    public void upgradeBuilding() {
+    }
+
+}
