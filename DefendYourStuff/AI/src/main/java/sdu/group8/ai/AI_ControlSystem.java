3a9f7cdae46c97390b42cea9cfaac0d43e9689a2
alexm15 <alexander.markussen@live.dk>
2017-04-25 15:09:26 +0200

    Added test for using assignAttackAndDodgeEnemyAI

--- DefendYourStuff/AI/src/main/java/sdu/group8/ai/AI_ControlSystem.java
+++ DefendYourStuff/AI/src/main/java/sdu/group8/ai/AI_ControlSystem.java
@@ -6,11 +6,20 @@
 
 package sdu.group8.ai;
 
+import sdu.group8.commonai.AI_Service;
+import sdu.group8.commoncharacter.Character;
+
 /**
  *
  * @author Alexander
  */
-public class AI_ControlSystem 
+public class AI_ControlSystem implements AI_Service
 {
+
+    @Override
+    public Character assignAttackAndDodgeEnemyAI(Character enemy) {
+        enemy.setX(1);
+        return enemy;
+    }
 
 }
--- DefendYourStuff/AI/src/test/java/sdu/group8/ai/AI_ControlSystemTest.java
+++ DefendYourStuff/AI/src/test/java/sdu/group8/ai/AI_ControlSystemTest.java
@@ -11,6 +11,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import sdu.group8.common.ability.Ability;
+import sdu.group8.common.data.Position;
+import sdu.group8.common.entity.CollisionType;
+import sdu.group8.common.entity.Entity;
+import sdu.group8.commoncharacter.Character;
 
 /**
  *
@@ -41,6 +46,23 @@ public class AI_ControlSystemTest {
     @org.junit.Test
     public void testSomeMethod() {
         
+    }
+
+    /**
+     * Test of assignAttackAndDodgeEnemyAI method, of class AI_ControlSystem.
+     */
+    @Test
+    public void testAssignAttackAndDodgeEnemyAI() {
+        AI_ControlSystem aiControl = new AI_ControlSystem();
+        Character someEnemy = new Character(0, 0, 0, null, null, new Position(0, 0), CollisionType.CIRLCE, new Ability[0]) {
+            @Override
+            public void collision(Entity otherEntity) {
+            }
+        };
+        float previousX = someEnemy.getX();
+        Character changedEnemy = aiControl.assignAttackAndDodgeEnemyAI(someEnemy);
+        
+        assertNotEquals(previousX, changedEnemy.getX(), 0);
     }
     
 }
