8258afaeaba0687c533f58cdc071f27271b89021
karim møller <Karimkuku@gmail.com>
2017-04-28 14:21:11 +0200

    deleted sout

--- DefendYourStuff/Enemy/src/main/java/sdu/group8/enemy/EnemyController.java
+++ DefendYourStuff/Enemy/src/main/java/sdu/group8/enemy/EnemyController.java
@@ -31,12 +31,12 @@ public class EnemyController implements IGameProcessingService, IGamePluginServi
     public void process(GameData gameData, World world) {
         
         float basePosX = (world.getChunkMiddle().getDimension().getWidth() / 2);
-        System.out.println("base: " + basePosX);
+
         for (Entity entityEnemy : world.getEntities(MediumEnemy.class)) {
             Character enemy = (Character) entityEnemy;
             
             float horizontalPos = enemy.getX();
-            System.out.println("X: " + enemy.getX());
+
             if (enemy.getX() < basePosX) {
                 horizontalPos += enemy.getMoveSpeed() * gameData.getDelta();
                 enemy.getImage().setReversed(true);
