10c362d89ce8c8efc4250e9b0338fc348e36ba39
karim møller <Karimkuku@gmail.com>
2017-05-09 14:08:05 +0200

    java doc i IPreStartPluginService

--- DefendYourStuff/Common/src/main/java/sdu/group8/common/services/IPreStartPluginService.java
+++ DefendYourStuff/Common/src/main/java/sdu/group8/common/services/IPreStartPluginService.java
@@ -12,5 +12,10 @@ import sdu.group8.common.data.GameData;
  * @author joach
  */
 public interface IPreStartPluginService {
+    /**
+     * This metode is call before the normal start metode and is used for componets that
+     * need to setup before the normal components.
+     * @param gameData Contains Game data such as window size, Delta Time etc.
+     */
     void preStart(GameData gameData);
 }
