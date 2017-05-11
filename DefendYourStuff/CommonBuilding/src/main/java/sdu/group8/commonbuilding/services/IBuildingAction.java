97faedeb69e8933964f6ded02fee75decd3c3ad1
alexm15 <alexander.markussen@live.dk>
2017-05-11 09:11:10 +0200

    Added JavaDoc

--- DefendYourStuff/CommonBuilding/src/main/java/sdu/group8/commonbuilding/services/IBuildingAction.java
+++ DefendYourStuff/CommonBuilding/src/main/java/sdu/group8/commonbuilding/services/IBuildingAction.java
@@ -8,9 +8,20 @@ package sdu.group8.commonbuilding.services;
 import sdu.group8.common.entity.Entity;
 
 /**
- *
+ * Interface used by other entities to make them perform building specific 
+ * actions related to the building they have collided with.
  * @author Alexander
  */
 public interface IBuildingAction {
+    
+    /**
+     * 
+     * Precondition: Collision have occured between an entity and a building.
+     * Building calls buildingAction on that entity.
+     * 
+     * Postcondition: The other entity has performed it's specific building
+     * action.
+     * @param building the building collided with 
+     */
     void buildingAction(Entity building);
 }
