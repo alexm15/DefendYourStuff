b43813919bb71db54fa9c765c95591c8f70d2a8b
alexm15 <alexander.markussen@live.dk>
2017-05-11 09:05:45 +0200

    Changed IBuildingAction method execute() to buildingAction(Entity building)

--- DefendYourStuff/Player/src/main/java/sdu/group8/player/Player.java
+++ DefendYourStuff/Player/src/main/java/sdu/group8/player/Player.java
@@ -19,6 +19,7 @@ import sdu.group8.common.entity.Entity;
 import sdu.group8.common.weapon.Weapon;
 import sdu.group8.commonenemy.IEnemyAction;
 import sdu.group8.commonability.services.AbilitySPI;
+import sdu.group8.commonbuilding.services.IBuildingAction;
 import sdu.group8.commonplayer.IPlayer;
 import sdu.group8.commonplayer.IPlayerAction;
 
@@ -26,7 +27,7 @@ import sdu.group8.commonplayer.IPlayerAction;
  *
  * @author joach
  */
-public class Player extends Character implements IPlayer, IEnemyAction {
+public class Player extends Character implements IPlayer, IEnemyAction, IBuildingAction {
 
     private Weapon weapon;
     private Position aimPoint;
@@ -87,6 +88,10 @@ public class Player extends Character implements IPlayer, IEnemyAction {
     @Override
     public HealthSystem getHealthSystem() {
         return this.health;
+    }
+
+    @Override
+    public void buildingAction(Entity building) {
     }
 
 }
