f07074ceb27f4715d1ef03eb705ea2984e8e4b3d
alexm15 <alexander.markussen@live.dk>
2017-05-12 10:56:17 +0200

    Removed CollisionContainer and EntityType

--- DefendYourStuff/Common/src/main/java/sdu/group8/common/entity/Item.java
+++ DefendYourStuff/Common/src/main/java/sdu/group8/common/entity/Item.java
@@ -10,7 +10,6 @@ import sdu.group8.common.ability.AbilityContainer;
 import sdu.group8.common.ability.AbilityData;
 import sdu.group8.common.data.Dimension;
 import sdu.group8.common.data.Position;
-import sdu.group8.common.collision.CollisionContainer;
 
 /**
  *
