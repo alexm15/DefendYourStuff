d5d5a2e6aa918fce1d96682352815b9dca1b02bb
Joachim Hejlesen <johej15@student.sdu.dk>
2017-05-15 18:47:11 +0200

    Added code so Abilities get removed when colliding with Building, Enemy, Player

--- DefendYourStuff/Ability/src/main/java/sdu/group8/ability/types/RangedAbility.java
+++ DefendYourStuff/Ability/src/main/java/sdu/group8/ability/types/RangedAbility.java
@@ -13,14 +13,18 @@ import sdu.group8.common.entity.CollisionType;
 import sdu.group8.common.entity.Entity;
 import sdu.group8.commonability.data.Ability;
 import sdu.group8.commonability.data.EffectContainer;
+import sdu.group8.commonbuilding.data.Building;
+import sdu.group8.commonbuilding.services.IBuildingAction;
 import sdu.group8.commonenemy.Enemy;
 import sdu.group8.commonenemy.IEnemyAction;
+import sdu.group8.commonplayer.IPlayer;
+import sdu.group8.commonplayer.IPlayerAction;
 
 /**
  *
  * @author Martin
  */
-public class RangedAbility extends Ability implements IEnemyAction {
+public class RangedAbility extends Ability implements IEnemyAction, IPlayerAction, IBuildingAction {
 
     public RangedAbility(float expiration, float moveSpeed, float weight, DamageRange damageRange, String imageURL, Dimension dimension, Direction direction, Position pos, CollisionType collisionType, EffectContainer effectContainer, Entity owner, boolean aimable) {
         super(expiration, moveSpeed, weight, damageRange, imageURL, dimension, direction, pos, collisionType, effectContainer, owner, aimable);
@@ -35,6 +39,24 @@ public class RangedAbility extends Ability implements IEnemyAction {
     @Override
     public void enemyAction(Entity enemy) {
         if (this.owner instanceof Enemy) {
+            
+        } else {
+            this.setExpiration(0);
+        }
+    }
+
+    @Override
+    public void playerAction(Entity player) {
+        if (this.owner instanceof IPlayer) {
+            
+        } else {
+            this.setExpiration(0);
+        }
+    }
+
+    @Override
+    public void buildingAction(Entity building) {
+        if (this.owner instanceof Building) {
             
         } else {
             this.setExpiration(0);
