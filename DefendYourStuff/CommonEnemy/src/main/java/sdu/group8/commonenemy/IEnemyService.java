47294b64423a80e9624f7d846126323d56c353c9
karim møller <Karimkuku@gmail.com>
2017-05-19 23:07:51 +0200

    Refactored commonEnemy

--- DefendYourStuff/CommonEnemy/src/main/java/sdu/group8/commonenemy/IEnemyService.java
+++ DefendYourStuff/CommonEnemy/src/main/java/sdu/group8/commonenemy/IEnemyService.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sdu.group8.commonenemy;
 
 import sdu.group8.common.data.GameData;
 import sdu.group8.common.data.Position;
 import sdu.group8.common.data.World;
 
-/**
- *
- * @author Martin
- */
 public interface IEnemyService {
     void createMediumEnemy(World world, GameData gameData, Position position);
     void createBigEnemy(World world, GameData gameData, Position position);
