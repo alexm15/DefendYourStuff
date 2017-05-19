770e1dfef2aabc90f3242cdae1bfa66e0ed888e7
karim møller <Karimkuku@gmail.com>
2017-05-19 23:11:47 +0200

    Refactored commonAlly + commonCharacter

--- DefendYourStuff/CommonAlly/src/main/java/sdu/group8/commonally/IAllyService.java
+++ DefendYourStuff/CommonAlly/src/main/java/sdu/group8/commonally/IAllyService.java
@@ -9,20 +9,6 @@ import sdu.group8.common.entity.Entity;
 
 public interface IAllyService {
     /**
-     * Retrieves an Allied NPC object.
-     * @param <C> a subtype of Character
-     * @param id the id of the Allied NPC
-     * @param world the collection of entities to search for the Allied NPC.
-     * @return the Allied NPC object
-     */
-    <C extends Character> C getAlly(UUID id, World world);
-    /**
-     * Retrieves the class of the Allied NPC object
-     * @param <C> a subtype of Character
-     * @return Allied NPC class that extends Character.
-     */
-    <C extends Character> C getAllyClass();
-    /**
      * Creates an Allied NPC through another entity in the game.
      * @param <C> a subtype of Character
      * @param creator the entity who creates the Allied NPC.
--- DefendYourStuff/CommonCharacter/src/main/java/sdu/group8/commoncharacter/Character.java
+++ DefendYourStuff/CommonCharacter/src/main/java/sdu/group8/commoncharacter/Character.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sdu.group8.commoncharacter;
 
 import sdu.group8.commonability.data.Ability;
@@ -16,10 +12,7 @@ import sdu.group8.common.data.World;
 import sdu.group8.common.entity.CollisionType;
 import sdu.group8.common.entity.MovingEntity;
 
-/**
- *
- * @author Martin
- */
+
 public abstract class Character extends MovingEntity {
 
     protected HealthSystem health;
