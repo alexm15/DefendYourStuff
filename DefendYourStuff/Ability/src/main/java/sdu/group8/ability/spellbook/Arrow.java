e27210b03a5928aaf693c2a76aad594854464a4f
karim møller <Karimkuku@gmail.com>
2017-05-19 23:24:04 +0200

    Refactored ability.spellbook

--- DefendYourStuff/Ability/src/main/java/sdu/group8/ability/spellbook/Arrow.java
+++ DefendYourStuff/Ability/src/main/java/sdu/group8/ability/spellbook/Arrow.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sdu.group8.ability.spellbook;
 
 import sdu.group8.ability.types.RangedAbility;
@@ -14,14 +9,10 @@ import sdu.group8.common.data.Position;
 import sdu.group8.common.entity.CollisionType;
 import sdu.group8.common.entity.Entity;
 
-/**
- *
- * @author Martin
- */
 public class Arrow extends RangedAbility {
-    
+
     public Arrow(Entity owner, float x, float y, boolean directionLeft) {
-        super(1f, 250f, 5f, new DamageRange(10,20), "abilities/fireball.png", new Dimension(30,30,15), new Direction(directionLeft), new Position(x,y), CollisionType.CIRCLE, new EffectContainer(), owner, true);
+        super(1f, 250f, 5f, new DamageRange(10, 20), "abilities/fireball.png", new Dimension(30, 30, 15), new Direction(directionLeft), new Position(x, y), CollisionType.CIRCLE, new EffectContainer(), owner, true);
     }
 
     @Override
