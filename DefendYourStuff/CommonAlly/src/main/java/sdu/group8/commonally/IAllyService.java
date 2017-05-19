87ed3b9aab8cbefe35442d2f7bc571faabdfbf4c
karim møller <Karimkuku@gmail.com>
2017-05-19 23:08:22 +0200

    Refactored commonAlly

--- DefendYourStuff/CommonAlly/src/main/java/sdu/group8/commonally/IAllyService.java
+++ DefendYourStuff/CommonAlly/src/main/java/sdu/group8/commonally/IAllyService.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sdu.group8.commonally;
 
 import java.util.UUID;
@@ -10,10 +6,7 @@ import sdu.group8.common.data.World;
 import sdu.group8.commoncharacter.Character;
 import sdu.group8.common.entity.Entity;
 
-/**
- *
- * @author Alexander
- */
+
 public interface IAllyService {
     /**
      * Retrieves an Allied NPC object.
