153b5217c8af1c552a890f63b374ba51976b7e97
karim møller <Karimkuku@gmail.com>
2017-05-19 23:12:42 +0200

    Refactored commonMap

--- DefendYourStuff/CommonMap/src/main/java/sdu/group8/commonmap/IMapUpdate.java
+++ DefendYourStuff/CommonMap/src/main/java/sdu/group8/commonmap/IMapUpdate.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sdu.group8.commonmap;
 
-import sdu.group8.common.data.GameData;
 import sdu.group8.common.data.World;
 
-/**
- * 
- * @author Martin
- */
 public interface IMapUpdate {
     /**
      * Updates either the left side or right side of the chunk arrays with a new chunk.
