e7d27fca3032da2269dca73e8491880a4235fd57
karim møller <Karimkuku@gmail.com>
2017-05-19 23:23:14 +0200

    Refactored ability.data

--- DefendYourStuff/Ability/src/main/java/sdu/group8/ability/data/ArrowData.java
+++ DefendYourStuff/Ability/src/main/java/sdu/group8/ability/data/ArrowData.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sdu.group8.ability.data;
 
 import sdu.group8.commonability.data.AbilityData;
 import sdu.group8.commonability.data.AbilityKey;
 
-/**
- *
- * @author Martin
- */
+
 public class ArrowData extends AbilityData {
     
     public ArrowData(AbilityKey key) {
--- DefendYourStuff/Ability/src/main/java/sdu/group8/ability/data/FireballData.java
+++ DefendYourStuff/Ability/src/main/java/sdu/group8/ability/data/FireballData.java
@@ -3,10 +3,7 @@ package sdu.group8.ability.data;
 import sdu.group8.commonability.data.AbilityData;
 import sdu.group8.commonability.data.AbilityKey;
 
-/**
- *
- * @author joach
- */
+
 public class FireballData extends AbilityData {
     
     public FireballData(AbilityKey key) {
